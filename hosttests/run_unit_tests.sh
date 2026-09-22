a4ebf7298154cd6536bdc4d6e92115d8ac6bcb62
omjoshi0925 <omjoshi823@gmail.com>
2026-09-22 15:46:20 -0700

    Tests: cinematic unit suite joins the asset-free runner

--- hosttests/run_unit_tests.sh
+++ hosttests/run_unit_tests.sh
@@ -4,9 +4,9 @@ set -e
 cd "$(dirname "$0")"
 SRC=../NativePort/Sources
 CXX=${CXX:-c++}
-ENGINE="$SRC/BDAEModel.cpp $SRC/Level.cpp $SRC/Character.cpp $SRC/Combat.cpp $SRC/GameFlow.cpp $SRC/Script.cpp $SRC/Audio.cpp"
+ENGINE="$SRC/BDAEModel.cpp $SRC/Level.cpp $SRC/Character.cpp $SRC/Combat.cpp $SRC/GameFlow.cpp $SRC/Script.cpp $SRC/Audio.cpp $SRC/Cinematic.cpp"
 fail=0
-for t in math trigger config_parser gameflow wav; do
+for t in math trigger config_parser gameflow wav cinematic; do
   $CXX -std=c++17 -O2 -I "$SRC" -o "/tmp/stm_unit_$t" $ENGINE "${t}_unit_test.cpp"
   "/tmp/stm_unit_$t" | tail -1 || fail=1
 done
